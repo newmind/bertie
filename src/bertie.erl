8e6a11b987beca60093c38ba0f495c692ba92d3d
jgkim <jgkim@n3n.co.kr>
2017-10-22 13:16:04 +0900

    bitcask

--- src/bertie.erl
+++ src/bertie.erl
@@ -1,5 +1,21 @@
 -module(bertie).
 -export([start/0]).
 
-start() -> io:format("Hello my name is Bertie~n").
+start() ->
+    Handle = bitcask:open("bertie database", [read_write]),
+    N = fetch(Handle),
+    store(Handle, N+1),
+    io:format("Bertie has been run ~p times~n", [N]),
+    bitcask:close(Handle),
+    init:stop().
+
+store(Handle, N) ->
+    bitcask:put(Handle, <<"bertie_executions">>, term_to_binary(N)).
+
+fetch(Handle) ->
+    case bitcask:get(Handle, <<"bertie_executions">>) of
+        now_found -> 1;
+        {ok, Bin} -> binary_to_term(Bin);
+        _ -> 1
+    end.
 
